3dccf59eab08c7cd881fae7532450eb5b2252271
hsoares <hsoares@ciandt.com>
2022-05-25 13:47:58 -0300

    Ajuste teste novo

--- src/test/java/org/example/qazandodozero2/TesteDeviceFarmLogin.java
+++ src/test/java/org/example/qazandodozero2/TesteDeviceFarmLogin.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.remote.RemoteWebElement;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class DeviceFarmLogin {
+public class TesteDeviceFarmLogin {
 
     private static ScreenLogin ScreenLogin;
     private static AppiumDriver<RemoteWebElement> driver;
